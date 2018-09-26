e84e23d17f47aa2a967eba740a1b4b1f5bf27468
Simon Barak <barak.simon.as@gmail.com>
2018-09-26 19:40:11 +0200

    window picker

--- assets/js/strobo.js
+++ assets/js/strobo.js
@@ -5,48 +5,60 @@
 
 const stroboGallery = (galleryElement) => {
 
-  const slotA = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6'];
-  const slotB = ['b1', 'b2', 'b3', 'b4', 'b5'];
-  const slotC = ['c1', 'c2', 'c3', 'c4'];
-  //const allSlots = [slotA, slotB, slotC];
   const allSlots = Array.from(galleryElement.getElementsByClassName('window'));
-  const cadence = 2000;
+  const cadence = 500;
+  const numberChanger = 0; // TODO: nefined like first element
 
-  var numberChanger = 0; // TODO: nefined like first element
 
-  const runRandomChoose = (array) => {
-    numberChanger = (function (oldElement) {
+  allSlots.forEach((slot) => {
+    slot.addEventListener('mouseover', function () {
+      slot.classList.remove('stop')
+    }, false)
 
-      const offElement = oldElement ? oldElement.classList.remove('active') : undefined;
-      const newElement = array[randomNumber(array.length)];
-      const activeElement = newElement.classList.add('active');
-      return newElement;
+    slot.addEventListener('mouseout', function () {
+      slot.classList.add('stop')
+    }, false)
+  })
 
-    }(numberChanger));
 
-    return numberChanger
-  }
 
-  const runOrderChoose = (array) => (numberChanger) => {
+  // NOT IN USE
+  // const runRandomChoose = (array) => {
+  //
+  //   numberChanger = (function (oldElement) {
+  //
+  //     const offElement = oldElement ? oldElement.classList.remove('active') : undefined;
+  //     const newElement = array[randomNumber(array.length)];
+  //     const activeElement = newElement.classList.add('active');
+  //     return newElement;
+  //
+  //   }(numberChanger));
+  //
+  //   return numberChanger
+  // }
 
-    numberChanger = (function (oldNumber) {
-      console.log('n: ' + oldNumber);
-      const newNumber = oldNumber < (array.length - 1) ? oldNumber + 1 : 0;
-      console.log('o: ' + newNumber);
-      return newNumber;
-    }(numberChanger));
-
-    return numberChanger
-  }
+  // NOT IN USE
+  // const runOrderChoose = (array) => (numberChanger) => {
+  //
+  //   numberChanger = (function (oldNumber) {
+  //     const newNumber = oldNumber < (array.length - 1) ? oldNumber + 1 : 0;
+  //     return newNumber;
+  //   }(numberChanger));
+  //
+  //   return numberChanger
+  // }
 
   const runStrobo = (array) => {
-    const slotNumber = randomNumber(array.length);
-    const slotElement = array[slotNumber];
+
+    //const result = array.filter(object => console.l); //object.classList.contains('.active')
+    const result = array.filter((element) => element.classList.contains('stop'));
+    const slotNumber = randomNumber(result.length);
+    const slotElement = result[slotNumber];
     const childsElement =  Array.from(slotElement.childNodes);
     const childsNumber = randomNumber(childsElement.length);
     const childElement = childsElement[childsNumber];
 
-    const offslots = array.forEach((element) => {
+    const offslots = result.forEach((element) => {
       element.classList.remove('active');
     });
     const offchilds = childsElement.forEach((element) => {
@@ -56,16 +68,16 @@ const stroboGallery = (galleryElement) => {
     const activeChild = childElement.classList.add('active');
   }
 
-  // var loop = (cadence) => {
-  //   return begin = setInterval(function() {
-  //     runStrobo(allSlots);
-  //   }, cadence);
-  // };
-  //
-  // loop(cadence);
+  var loop = (cadence) => {
+    return begin = setInterval(function() {
+      runStrobo(allSlots);
+    }, cadence);
+  };
+
+  loop(cadence);
 
 
-  var addMouseWheelEventListener = function (scrollHandler) {
+  const addMouseWheelEventListener = function (scrollHandler) {
     if (window.addEventListener) {
       // IE9+, Chrome, Safari, Opera
       window.addEventListener("mousewheel", function() {
