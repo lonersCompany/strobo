d37b90d142628c154be2f04337022b0b4933eb27
Schittaci-air <barak.simon.as@gmail.com>
2020-07-20 15:17:49 +0200

    fix wrong path

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,19 +1,42 @@
----
----
-window.onload = function () {
+window.onload = function() {
+  const toggleFullScreen = sliderEl => {
+    const zoomMode = sliderEl.classList.toggle("swiper-zoom-in");
+    console.log(zoomMode);
+
+    switch (zoomMode) {
+      case "false":
+        sliderEl.classList.add("swiper-zoom-in");
+        sliderEl.scrollIntoView("alignToTop");
+        break;
+
+      default:
+        sliderEl.classList.remove("swiper-zoom-in");
+        sliderEl.setAttribute("zoom", "false");
+        window.scrollTo(0, 0);
+    }
+  };
+
+  const initFullScreen = sliderEl => {
+    // const slidesArr = Array.from(sliderEl.querySelectorAll("img"));
+
+    sliderEl.addEventListener("click", () => toggleFullScreen(sliderEl));
+  };
+
+  const sliderEl = document.querySelector(".swiper-controler");
+  if (sliderEl) initFullScreen(sliderEl);
+
   //initialize swiper when document ready
-  var swiperDetail = new Swiper ('.swiper-controler', {
-    direction: 'horizontal',
+  var swiperDetail = new Swiper(".swiper-controler", {
+    direction: "horizontal",
     navigation: {
-      nextEl: '.swiper-next',
-      prevEl: '.swiper-prev',
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev"
     },
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
+      el: ".swiper-pagination",
+      type: "bullets"
     },
     effect: "slide",
-    loop: true,
-    autoHeight: true
-  })
+    loop: true
+  });
 };
